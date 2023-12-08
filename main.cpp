3757b181b488a747d2848b9acafdc773f5c98b8f
Ayden Chance <achance6@jhu.edu>
2023-12-08 14:52:43 -0500

    Basic hangman functionality. Can't handle duplicate characters in answer yet.

--- main.cpp
+++ main.cpp
@@ -5,15 +5,34 @@ int main() {
 	using std::string;
 	using std::cout, std::cin;
 
-	string answer{ "Hangman" };
-
-	cout << "Guess a letter: ";
-	char guess{};
-	cin >> guess;
+	string answer{ "hangman" };
 
 	string display{};
-	for (int i{ 0 }; i < answer.length(); ++i) {
-		display += '_';
+	for (int i{ 0 }; i < answer.length(); ++i) display += '_';
+	cout << "I'm thinking of a word with " << answer.length() << " letters.\n";
+	cout << display << '\n';
+
+	int lives{ 10 };
+	while (lives > 0 && display.find('_') != string::npos) {
+		cout << "Guess a letter (# to quit): ";
+		char guess{};
+		cin >> guess;
+		if (guess == '#') exit(0);
+		
+		std::size_t loc{ answer.find(guess) };
+		if (loc != string::npos) {
+			display.replace(loc, 1, 1, guess);
+		}
+		else {
+			cout << "Incorrect.\n";
+			lives--;
+			cout << "You have " << lives << " lives remaining.\n";
+		}
+
+		cout << display << '\n';
+		
 	}
+
+	cout << (lives > 0 ? "You win!" : "You lose!");
 	return 0;
 }
