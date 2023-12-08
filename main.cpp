27a6564c3ebc388bcc10774d9435044d6cddd48e
Ayden Chance <achance6@jhu.edu>
2023-12-08 15:08:14 -0500

    Now handled duplicate letters

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@ int main() {
 	using std::string;
 	using std::cout, std::cin;
 
-	string answer{ "hangman" };
+	string answer{ "hangmann" };
 
 	string display{};
 	for (int i{ 0 }; i < answer.length(); ++i) display += '_';
+
 	cout << "I'm thinking of a word with " << answer.length() << " letters.\n";
 	cout << display << '\n';
 
@@ -20,6 +21,12 @@ int main() {
 		if (guess == '#') exit(0);
 		
 		std::size_t loc{ answer.find(guess) };
+
+		// Handles case where duplicate letters exist in answer
+		while (loc != string::npos && display.at(loc) != '_') {
+			loc = answer.find(guess, loc + 1);
+		}
+
 		if (loc != string::npos) {
 			display.replace(loc, 1, 1, guess);
 		}
