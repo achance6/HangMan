5f44b44c0ccdcbdbb2070e9ad11f9e15d3411fdd
Ayden Chance <achance6@jhu.edu>
2023-12-12 10:28:03 -0500

    Added doxygen comment to handleGuess

--- hangman.h
+++ hangman.h
@@ -9,11 +9,14 @@
 void play();
 
 /**
- * 
+ * Handles the given guess by comparing it to the given state of the answer
+ * and modifying the given display reference. Needs a guessHistory in order
+ * to not penalize guesses that have already been made. 
  *
- * @param answer
- * @param display
- * @param guessHistory
+ * @param answer Correct answer to compare guess to
+ * @param display Current display of letters
+ * @param guessHistory History of guesses user has made. Will add current guess
+ * to the history
  * @return True if guess is correct, false otherwise
  */
 bool handleGuess(std::string_view answer, 
