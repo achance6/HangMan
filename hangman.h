36229d09a9bca0c6a74020061143935ccd51b37a
Ayden Chance <achance6@jhu.edu>
2023-12-12 10:34:32 -0500

    Added doxygen comments to getWords and getAnswer

--- hangman.h
+++ hangman.h
@@ -13,11 +13,11 @@ void play();
  * and modifying the given display reference. Needs a guessHistory in order
  * to not penalize guesses that have already been made. 
  *
- * @param answer Correct answer to compare guess to
- * @param display Current display of letters
+ * @param answer Correct answer to compare guess to.
+ * @param display Current display of letters.
  * @param guessHistory History of guesses user has made. Will add current guess
- * to the history
- * @return True if guess is correct, false otherwise
+ * to the history.
+ * @return True if guess is correct, false otherwise.
  */
 bool handleGuess(std::string_view answer, 
 	std::string& display,
@@ -26,15 +26,16 @@ bool handleGuess(std::string_view answer,
 /**
  * Generate a new hangman answer from the English dictionary.
  *
- * @return english word
+ * @return English word.
  */
 std::string genAnswer();
 
 /**
- * .
+ * Loads words from dictionary txt file into program vector if it has not
+ * already been done in the program. Otherwise just returns the vector.
  *
- * @param lowerLim
- * @return
+ * @param lowerLim Lower limit of word length to add to vector. Default 7.
+ * @return Vector of dictionary words. Quite large.
  */
 std::vector<std::string> getWords(int lowerLim = 7);
 
