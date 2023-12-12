a68c1a3d3d7c790db53fa2760c312cdecd0c3ea8
Ayden Chance <achance6@jhu.edu>
2023-12-12 10:43:36 -0500

    Added doxygen comments for io.cpp

--- io.h
+++ io.h
@@ -1,5 +1,21 @@
 #pragma once
 
+/**
+ * Clears out cin stream.
+ *
+ */
 void ignoreLine();
+
+/**
+ * Get guess from user in form of a character.
+ *
+ * @return User's guess.
+ */
 char getGuess();
+
+/**
+ * Get command from user.
+ *
+ * @return User given command.
+ */
 char getCmd();
